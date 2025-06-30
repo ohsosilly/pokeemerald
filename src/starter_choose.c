5fc40f9d50a5a8beffd33489fcc2148ce83465be
char <126907685+ohsosilly@users.noreply.github.com>
2025-06-30 21:26:11 +0000

    Pending changes exported from your codespace

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -114,7 +114,7 @@ static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
     SPECIES_TREECKO,
     SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_RALTS,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
